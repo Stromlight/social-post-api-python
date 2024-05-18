f9fbf6944c69314a817b7697c9effd8edd7bb294
Stromlight <163277888+Stromlight@users.noreply.github.com>
2024-05-18 09:29:13 +0545

    Update setup.py

--- setup.py
+++ setup.py
@@ -37,3 +37,21 @@ setup(name=PACKAGE_NAME,
       install_requires=INSTALL_REQUIRES,
       packages=find_packages()
       )
+LICENSE = 'Apache License 2.0'
+DESCRIPTION = 'Social Media API: Schedule posts, get analytics, manage comments for Instagram, X/Twitter, Facebook, YouTube, LinkedIn, Google Business Profile, Pinterest, Telegram, TikTok, and Reddit.'
+KEYWORDS = "Ayrshare, Social Media API, Social Networks, Social Media Management, Social API, Social Posting, Social Analytics, Social Automation, Agency Social, Multiple User Posting, Instagram, YouTube API, X API, Twitter API, Facebook API, LinkedIn API, Reddit API, Telegram API, Pinterest API, Google Business Profile API, TikTok API"
+LONG_DESCRIPTION = (HERE / "README.md").read_text()
+LONG_DESC_TYPE = "text/markdown"
+PROJECT_URLS={
+    'Documentation': 'https://docs.ayrshare.com',
+    'Source': 'https://github.com/ayrshare/social-post-api-python',- my code/* all social medias platform link- stromlightgithub- for- purpose of humanity,i direct comand to receive all comment section of social media apps above mentions.,  */.,
+*getting access to all social media- to - stromlight- github- account as" humanity" title,
+*getting pass for redirecting comment section
+* law and rules under human rights - of- right to speech.,
+/* world human law tells right in rule is humanity.,
+* setting law as authorizer ,
+.,- stromlight- github- educatinhub., project.html run.,
+with.,/* NLP - I code of comand to the excess to all social media app.,
+/*.,
+*can edit and change.,
+/*
